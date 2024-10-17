ed0c2e0afb7fc55143d57e4f9f4b8fd24c07154a
Celso Cardenas <cardenascelso9@gmail.com>
2024-10-17 13:29:28 -0600

    fix useHasExternalWalletProvider to the full version

--- src/providers/WalletProvider/useHasExternalWalletProvider.ts
+++ src/providers/WalletProvider/useHasExternalWalletProvider.ts
@@ -1,29 +1,31 @@
-// import { ChainType } from '@lifi/sdk';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { EVMExternalContext } from './EVMExternalContext';
 import { SVMExternalContext } from './SVMExternalContext';
+import { ChainTypeCustom } from '../WidgetProvider/types';
 
 interface ExternalWalletProvider {
   hasExternalProvider: boolean;
+  providers: ChainTypeCustom[]; 
 }
 
 export function useHasExternalWalletProvider(): ExternalWalletProvider {
   const hasExternalEVMContext = useContext(EVMExternalContext);
   const hasExternalSVMContext = useContext(SVMExternalContext);
 
-  // const providers = useMemo(() => {
-  //   const providers = [];
-  //   if (hasExternalEVMContext) {
-  //   //   providers.push(ChainType.EVM);
-  //   }
-  //   if (hasExternalSVMContext) {
-  //   //   providers.push(ChainType.SVM);
-  //   }
-  //   return providers;
-  // }, [hasExternalEVMContext, hasExternalSVMContext]);
+  // create an array of providers based on the context values 
+  const providers = useMemo(() => {
+    const providersList: ChainTypeCustom[] = [];
+    if (hasExternalEVMContext) {
+      providersList.push(ChainTypeCustom.EVM);
+    }
+    if (hasExternalSVMContext) {
+      providersList.push(ChainTypeCustom.SVM);
+    }
+    return providersList;
+  }, [hasExternalEVMContext, hasExternalSVMContext]);
 
   return {
     hasExternalProvider: hasExternalEVMContext || hasExternalSVMContext,
-    // providers,
+    providers, 
   };
 }
