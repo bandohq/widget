d4599cff682d17df3792fcfdd4503679d5653094
Celso Cardenas <cardenascelso9@gmail.com>
2024-10-23 20:16:29 -0600

    add idProject from walletConnect

--- src/config/walletConnect.ts
+++ src/config/walletConnect.ts
@@ -1,6 +1,5 @@
 import type { WalletConnectParameters } from 'wagmi/connectors';
 
 export const defaultWalletConnectConfig: WalletConnectParameters = {
-  // change for Bando's project
-  projectId: '5432e3507d41270bee46b7b85bbc2ef8', // get from https://cloud.walletconnect.com
+  projectId: '00fcded02606b78df1f2f732def1d79f', // get from https://cloud.walletconnect.com
 };
