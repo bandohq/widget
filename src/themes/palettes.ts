53a27095102ddc5924a796f401a4f5de948d6a58
Celso Cardenas <cardenascelso9@gmail.com>
2024-10-28 16:54:38 -0600

    configure theme

---
+++ src/themes/palettes.ts
@@ -0,0 +1,53 @@
+export const palette = {
+    primary: {
+      main: '#53C5A5',
+    },
+    secondary: {
+      main: '#B785FA',
+    },
+    success: {
+      main: '#0AA65B',
+    },
+    warning: {
+      main: '#FFCC00',
+    },
+    error: {
+      main: '#E5452F',
+    },
+    info: {
+      main: '#297EFF',
+    },
+    common: {
+      black: '#000000',
+      white: '#ffffff',
+    },
+    grey: {
+      '200': '#E3E8E7',
+      '400': '#C7D1CE',
+      '700': '#616161',
+      '800': '#738C85',
+    },
+  }
+  
+  export const paletteDark = {
+    background: {
+      paper: '#212121',
+      default: '#121212',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#bbbbbb',
+    },
+  }
+  
+  export const paletteLight = {
+    background: {
+      paper: '#ffffff',
+      default: '#ffffff',
+    },
+    text: {
+      primary: '#000000',
+      secondary: '#747474',
+    },
+  }
+  
