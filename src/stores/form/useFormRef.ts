2a13ffac034665d6e8b9d5c5dad9e04b476b6ec2
Celso Cardenas <cardenascelso9@gmail.com>
2024-11-01 22:18:36 -0600

    increase form store

---
+++ src/stores/form/useFormRef.ts
@@ -0,0 +1,41 @@
+import { useImperativeHandle } from 'react'
+import { useWidgetConfig } from '../../providers/WidgetProvider/WidgetProvider.js'
+import { useBookmarkActions } from '../../stores/bookmarks/useBookmarkActions.js'
+import { formDefaultValues } from '../../stores/form/createFormStore.js'
+import type { FormRef } from '../../types/widget.js'
+import type { FormStoreStore, GenericFormValue } from './types.js'
+
+export const useFormRef = (formStore: FormStoreStore, formRef?: FormRef) => {
+  const { setSelectedBookmark } = useBookmarkActions()
+  const { hiddenUI } = useWidgetConfig()
+
+  useImperativeHandle(
+    formRef,
+    () => {
+      const sanitizeValue: {
+        [key: string]: (value: any) => GenericFormValue
+      } = {
+        fromAmount: (value) =>
+          (typeof value === 'number' ? value?.toPrecision() : value) ||
+          formDefaultValues.fromAmount,
+      }
+
+      return {
+        setFieldValue: (fieldName, value, options) => {
+          const sanitizedValue = (
+            sanitizeValue[fieldName] ? sanitizeValue[fieldName](value) : value
+          ) as GenericFormValue
+
+          const fieldValueOptions = options?.setUrlSearchParam
+            ? { isTouched: options?.setUrlSearchParam }
+            : undefined
+
+          formStore
+            .getState()
+            .setFieldValue(fieldName, sanitizedValue, fieldValueOptions)
+        },
+      }
+    },
+    [formStore, hiddenUI, setSelectedBookmark]
+  )
+}
