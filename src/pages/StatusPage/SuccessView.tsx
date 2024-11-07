1c6b9a0a8c91fb8122d8a17e4d7fd44cdba01a0d
Celso Cardenas <cardenascelso9@gmail.com>
2024-11-07 12:39:57 -0600

    fix success view

--- src/pages/StatusPage/SuccessView.tsx
+++ src/pages/StatusPage/SuccessView.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { AccordionDetails, Button, Typography } from "@mui/material";
+import { AccordionDetails, Avatar, Button, Typography } from "@mui/material";
 import {
   CustomAccordion,
   CustomAccordionSummary,
@@ -11,9 +11,17 @@ import {
 import { Check, CaretDown, Barcode } from "@phosphor-icons/react";
 import { palette } from "../../themes/palettes";
 import { navigationRoutes } from "../../utils/navigationRoutes";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 export const SuccessView = () => {
+  const [product, setProduct] = useState({
+    img_url: false,
+    name: "Product name",
+    price: 0,
+  });
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const gotoHome = () => {
     navigate(navigationRoutes.home);
@@ -23,19 +31,22 @@ export const SuccessView = () => {
       <IconWrapper bgColor={palette.primary.medium}>
         <Check size={50} />
       </IconWrapper>
-      <StatusTitle>Order completed</StatusTitle>
-      <StatusSubtitle>Thank you for your purchase.</StatusSubtitle>
+      <StatusTitle>{t("success.title.orderCompleted")}</StatusTitle>
+      <StatusSubtitle>{t("success.title.thanks")}</StatusSubtitle>
 
       <ProductBox>
-        <Barcode
-          size={32}
-          style={{ boxShadow: "0px 11px 32px -17px rgba(0,0,0,0.84)" }}
-        />
+        {!product.img_url ? (
+          <Avatar alt={product.name} sx={{ bgcolor: palette.primary.main }}>
+            NP
+          </Avatar>
+        ) : (
+          <Barcode size={50} />
+        )}
         <StatusTitle fontSize="26px">
-          {"Product name"} - {"$0.00"}
+          {product.name} - {"$0.00"}
         </StatusTitle>
         <StatusSubtitle fontSize="18px">
-          We have sent the gift card to your email.
+          {t("success.message.mailNotification")}
         </StatusSubtitle>
       </ProductBox>
 
@@ -45,7 +56,7 @@ export const SuccessView = () => {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Order details</Typography>
+          <Typography>{t("success.title.orderDetails")}</Typography>
         </CustomAccordionSummary>
         <AccordionDetails>
           <Typography>Contenido de la sección 1.</Typography>
@@ -58,7 +69,7 @@ export const SuccessView = () => {
         style={{ borderRadius: "30px" }}
         onClick={gotoHome}
       >
-        Discover more
+        {t("button.discover")}
       </Button>
     </>
   );
