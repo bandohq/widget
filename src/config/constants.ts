06a44b493e22904f55ce2cbe8c5aa5ea5645e3d6
Celso Cardenas <cardenascelso9@gmail.com>
2024-12-01 19:16:30 -0600

    add provisional bandoURL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,3 +4,5 @@ export const lifiExplorerUrl = 'https://bando.cool'
 
 // export const BANDO_API_URL = 'https://api.bando.cool/v1/'
 export const BANDO_API_URL = 'https://apidev.bando.cool/api/v1/'
+
+export const BANDO_API_URL_V2 = 'https://dev.bando.cool/api/v1/'
