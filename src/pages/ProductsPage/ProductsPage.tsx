5993eb28ae804b28994f9f0c7d61beb0948fb07a
g6s <guayabas@hey.com>
2024-12-01 19:29:33 -0600

    implement fuzzy search on grouped categories

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -16,12 +16,42 @@ export const ProductsPage = () => {
     url: "products/grouped/",
   });
   const { t } = useTranslation();
+  const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
   useHeader(t("header.spend"));
+
+  useEffect(() => {
+    console.log(data);
+    setFilteredData(data);
+  }, [data]);
 
   const handleMoreClick = (category) => {
     setSelectedCategory(category);
     navigate(`${navigationRoutes.products}/${category.productType}`);
+  };
+
+  const handleSearchChange = (query) => {  
+    // Use original data for searching
+    const fData = data?.products
+      ?.map((category) => {
+        // Filter brands within each category
+        const matchingBrands = category.brands.filter((item) =>
+          item.brandSlug.toLowerCase().includes(query.toLowerCase())
+        );
+  
+        // Return the category with the filtered brands
+        return {
+          ...category,
+          brands: matchingBrands,
+        };
+      })
+      // Filter out categories with no matching brands
+      ?.filter((category) => category.brands.length > 0);
+    setSearchQuery(query);
+  
+    // Update filteredData with the new filtered products
+    setFilteredData({ products: fData });
   };
 
   if (selectedCategory) {
@@ -34,7 +64,7 @@ export const ProductsPage = () => {
 
   return (
     <PageContainer>
-      <ProductSearch />
+      <ProductSearch onSearchChange={handleSearchChange} />
 
       {isPending
         ? Array.from(new Array(5)).map((_, index) => (
@@ -45,7 +75,7 @@ export const ProductsPage = () => {
               height="100px"
             />
           ))
-        : data?.products?.map((category) => (
+        : filteredData?.products?.map((category) => (
             <CategorySection
               key={category.productType}
               category={category}
