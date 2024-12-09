4e1352dae7ea84881454501ba73c5d31c88e58b4
Celso Cardenas <cardenascelso9@gmail.com>
2024-12-09 13:14:53 -0600

    clean routes file

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,18 +1,10 @@
 import type { RouteObject } from "react-router-dom";
 import { useRoutes } from "react-router-dom";
 import { NotFound } from "./components/NotFound";
-// import { ActiveTransactionsPage } from "./pages/ActiveTransactionsPage/ActiveTransactionsPage.js";
 import { LanguagesPage } from "./pages/LanguagesPage";
 import { MainPage } from "./pages/MainPage/MainPage.js";
-// import { RoutesPage } from "./pages/RoutesPage/RoutesPage.js";
-import { SelectChainPage } from "./pages/SelectChainPage/SelectChainPage.js";
-// import { SelectEnabledToolsPage } from "./pages/SelectEnabledToolsPage.js";
 import { SelectTokenPage } from "./pages/SelectTokenPage/SelectTokenPage.js";
-// import { SendToConfiguredWalletPage } from "./pages/SendToWallet/SendToConfiguredWalletPage.js";
 import { SettingsPage } from "./pages/SettingsPage/SettingsPage";
-// import { TransactionDetailsPage } from "./pages/TransactionDetailsPage/TransactionDetailsPage.js";
-// import { TransactionHistoryPage } from "./pages/TransactionHistoryPage/TransactionHistoryPage.js";
-// import { TransactionPage } from "./pages/TransactionPage/TransactionPage.js"
 import { navigationRoutes } from "./utils/navigationRoutes";
 import { StatusPage } from "./pages/StatusPage/StatusPage";
 import { ProductsPage } from "./pages/ProductsPage/ProductsPage";
@@ -36,14 +28,6 @@ const routes: RouteObject[] = [
     path: navigationRoutes.status,
     element: <StatusPage />,
   },
-  // {
-  //   path: `${navigationRoutes.settings}/${navigationRoutes.bridges}`,
-  //   element: <SelectEnabledToolsPage type="Bridges" />,
-  // },
-  // {
-  //   path: `${navigationRoutes.settings}/${navigationRoutes.exchanges}`,
-  //   element: <SelectEnabledToolsPage type="Exchanges" />,
-  // },
   {
     path: `${navigationRoutes.products}`,
     element: <ProductsPage />,
@@ -64,34 +48,6 @@ const routes: RouteObject[] = [
     path: navigationRoutes.fromToken,
     element: <SelectTokenPage formType="from" />,
   },
-  {
-    path: `${navigationRoutes.fromToken}?/${navigationRoutes.fromChain}`,
-    element: <SelectChainPage formType="from" />,
-  },
-  // {
-  //   path: navigationRoutes.routes,
-  //   element: <RoutesPage />,
-  // },
-  // {
-  //   path: navigationRoutes.activeTransactions,
-  //   element: <ActiveTransactionsPage />,
-  // },
-  // {
-  //   path: navigationRoutes.configuredWallets,
-  //   element: <SendToConfiguredWalletPage />,
-  // },
-  // {
-  //   path: navigationRoutes.transactionHistory,
-  //   element: <TransactionHistoryPage />,
-  // },
-  // {
-  //   path: `${navigationRoutes.transactionHistory}?/${navigationRoutes.routes}?/${navigationRoutes.transactionExecution}?/${navigationRoutes.transactionDetails}`,
-  //   element: <TransactionDetailsPage />,
-  // },
-  // {
-  //   path: `${navigationRoutes.routes}?/${navigationRoutes.activeTransactions}?/${navigationRoutes.transactionExecution}`,
-  //   element: <TransactionPage />,
-  // },
   {
     path: "*",
     element: <NotFound />,
