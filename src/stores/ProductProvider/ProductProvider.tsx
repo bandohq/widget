465570220643b4e28333ee00439222a40ca55b74
Celso Cardenas <cardenascelso9@gmail.com>
2024-12-16 23:33:17 -0600

    add brands to provider

--- src/stores/ProductProvider/ProductProvider.tsx
+++ src/stores/ProductProvider/ProductProvider.tsx
@@ -5,13 +5,20 @@ const ProductContext = createContext(null);
 
 export const ProductProvider = ({ children }) => {
   const [product, setProduct] = useState<Product>(null);
+  const [brand, setBrand] = useState(null);
 
   const updateProduct = (newProduct) => {
     setProduct(newProduct);
   };
 
+  const updateBrand = (newBrand) => {
+    setBrand(newBrand);
+  };
+
   return (
-    <ProductContext.Provider value={{ product, updateProduct }}>
+    <ProductContext.Provider
+      value={{ product, brand, updateProduct, updateBrand }}
+    >
       {children}
     </ProductContext.Provider>
   );
