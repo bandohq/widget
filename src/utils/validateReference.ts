7a890e6a9f77484783a63f38e1a20c263c73c67c
Celso Cardenas <cardenascelso9@gmail.com>
2025-01-12 10:15:54 -0600

    create right validate reference function

---
+++ src/utils/validateReference.ts
@@ -0,0 +1,20 @@
+import { readContract } from "@wagmi/core";
+import FulfillableRegistry from "@bandohq/contract-abis/abis/FulfillableRegistryV1.json";
+
+export const validateReference = async (chain, serviceID, referenceCode, config) => {
+  const FulfillableRegistryABI = FulfillableRegistry.abi.find(
+    (item) => item.name === "isRefValid"
+  );
+
+  const isValid = await readContract(config, {
+    address: chain?.protocol_contracts?.FulfillableRegistry,
+    abi: [FulfillableRegistryABI],
+    functionName: "isRefValid",
+    args: [serviceID, referenceCode],
+    chainId: chain?.id,
+  });
+
+  console.log("isValid", isValid);
+
+  return isValid;
+};
