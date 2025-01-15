350a9f704e6b40dcb9fc21bb8692a54ce1931f7b
Celso Cardenas <cardenascelso9@gmail.com>
2025-01-15 17:35:11 -0600

    fix alert styles

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,24 +1,39 @@
-import { Box, Collapse } from "@mui/material";
+import { Box, Collapse, Typography } from "@mui/material";
 import { useNotificationContext } from "../../providers/AlertProvider/AlertProvider";
 import { palette } from "../../themes/palettes";
+import { useEffect, useState } from "react";
 
 export const Alert = () => {
   const { notification } = useNotificationContext();
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    if (notification) {
+      setShowNotification(true);
+      const timer = setTimeout(() => {
+        setShowNotification(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
+
   return (
-    <div>
-      {notification && (
-        <Collapse in={notification !== null}>
-          <Box
-            sx={{
-              bgcolor: palette[notification?.type].main,
-              width: "100%",
-              p: 2,
-            }}
-          >
-            {notification?.message}
-          </Box>
-        </Collapse>
-      )}
-    </div>
+    <Collapse in={showNotification}>
+      <Box
+        sx={{
+          bgcolor: palette[notification?.type]?.main || "transparent",
+          width: "100%",
+          p: 2,
+        }}
+      >
+        <Typography
+          color={notification?.type === "warning" ? "black" : "white"}
+          align="center"
+          variant="body1"
+        >
+          {notification?.message}
+        </Typography>
+      </Box>
+    </Collapse>
   );
 };
