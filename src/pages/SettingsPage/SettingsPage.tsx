c9d431be3a4a2d166206590c3063c0553cf5cb49
Celso Cardenas <cardenascelso9@gmail.com>
2025-01-16 12:17:12 -0600

    clean status page

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -6,22 +6,11 @@ import { ResetSettingsButton } from "./ResetSettingsButton";
 import { SettingsList } from "./SettingsCard/SettingCard.style";
 import { SettingsCardAccordion } from "./SettingsCard/SettingsAccordian";
 import { ThemeSettings } from "./ThemeSettings";
-import { CardButton } from "../../components/Card/CardButton";
-import { useNavigate } from "react-router-dom";
-import { navigationRoutes } from "../../utils/navigationRoutes";
-import { Language } from "@mui/icons-material";
-import { CardValue } from "../../components/Card/CardButton.style";
 import { CountriesSetting } from "./CountriesSetting";
 
 export const SettingsPage = () => {
   const { t } = useTranslation();
   useHeader(t("header.settings"));
-
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(`/${navigationRoutes.status}`);
-  };
 
   return (
     <PageContainer bottomGutters>
@@ -30,13 +19,6 @@ export const SettingsPage = () => {
           <ThemeSettings />
           <LanguageSetting />
           <CountriesSetting />
-          <CardButton
-            onClick={handleClick}
-            icon={<Language />}
-            title={"Status"}
-          >
-            <CardValue>page</CardValue>
-          </CardButton>
         </SettingsCardAccordion>
       </SettingsList>
       <ResetSettingsButton />
