042f47f568129b547fa2211e2e0f563b7436f3fc
Celso Cardenas <cardenascelso9@gmail.com>
2025-01-21 13:28:16 -0600

    update country page to be reused on country selection

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -1,18 +1,24 @@
-import { List, ListItemIcon, Avatar, Chip } from "@mui/material";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { List, ListItemIcon, Avatar, Chip } from "@mui/material";
+import { useCountryContext } from "../stores/CountriesProvider/CountriesProvider";
 import { ListItemText } from "../components/ListItemText";
 import { PageContainer } from "../components/PageContainer";
 import { SettingsListItemButton } from "../components/SettingsListItemButton";
 import { useHeader } from "../hooks/useHeader";
-import { useCountryContext } from "../stores/CountriesProvider/CountriesProvider";
-import { useState } from "react";
 import { SearchBar } from "../components/SearchInput/SearchInput";
 
 export const CountryPage: React.FC = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const isProductPage = searchParams.get("productPage");
   const {
     removeCountry,
     restoreCountry,
+    selectCountry,
     blockedCountries,
     availableCountries,
     isCountryPending,
@@ -45,36 +51,44 @@ export const CountryPage: React.FC = () => {
         }}
       >
         {/* Blocked countries */}
-        {filteredBlockedCountries.map((country) => (
-          <SettingsListItemButton
-            key={country.iso_alpha2}
-            onClick={() => {
-              restoreCountry(country.iso_alpha2);
-            }}
-          >
-            <ListItemIcon>
-              <Avatar
-                src={country.flag_url}
-                alt={country.name}
-                sx={{ objectFit: "cover" }}
+        {!isProductPage &&
+          filteredBlockedCountries.map((country) => (
+            <SettingsListItemButton
+              key={country.iso_alpha2}
+              onClick={() => {
+                restoreCountry(country.iso_alpha2);
+              }}
+            >
+              <ListItemIcon>
+                <Avatar
+                  src={country.flag_url}
+                  alt={country.name}
+                  sx={{ objectFit: "cover" }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary={`${country.name} - ${country.iso_alpha2}`}
               />
-            </ListItemIcon>
-            <ListItemText primary={`${country.name} - ${country.iso_alpha2}`} />
-            <Chip
-              color="error"
-              label={t("countries.excluded")}
-              variant="outlined"
-              size="small"
-            />
-          </SettingsListItemButton>
-        ))}
+              <Chip
+                color="error"
+                label={t("countries.excluded")}
+                variant="outlined"
+                size="small"
+              />
+            </SettingsListItemButton>
+          ))}
 
         {/* Available countries */}
         {filteredAvailableCountries.map((country) => (
           <SettingsListItemButton
             key={country.iso_alpha2}
             onClick={() => {
-              removeCountry(country.iso_alpha2);
+              if (isProductPage) {
+                selectCountry(country.iso_alpha2);
+                navigate(-1);
+              } else {
+                removeCountry(country.iso_alpha2);
+              }
             }}
           >
             <ListItemIcon>
