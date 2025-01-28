7ece6a90038dd7c17eebeae1ae90f14d78dfb87b
Celso Cardenas <cardenascelso9@gmail.com>
2025-01-28 11:35:58 -0600

    change dev endpoint for prod

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,5 @@
 export const defaultMaxHeight = 686
 
-export const lifiExplorerUrl = 'https://bando.cool'
+export const bandoExplorerUrl = 'https://bando.cool'
 
-// export const BANDO_API_URL = 'https://api.bando.cool/v1/'
-export const BANDO_API_URL = 'https://apidev.bando.cool/api/v1/'
+export const BANDO_API_URL = 'https://api.bando.cool/v1/'
