2d8a8310c2b12131f499e2619403fb4aba28c107
g6s <guayabas@hey.com>
2025-01-28 19:31:23 -0600

    add index reference on package and export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { App as BandoWidget } from './App'
 export { useWallets, walletComparator } from './hooks/useWallets';
 export * from './config/versions.js'
+export * from './types/widget.js'
+export * from './types/events.js'
