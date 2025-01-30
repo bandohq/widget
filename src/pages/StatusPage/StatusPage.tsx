969584766e329d6d38124a551146dd61998859e3
Celso Cardenas <37398441+cecam@users.noreply.github.com>
2025-01-30 16:51:01 -0600

    Update src/pages/StatusPage/StatusPage.tsx
    Co-authored-by: Domingo Suarez Torres <domingo.suarez@gmail.com>

--- src/pages/StatusPage/StatusPage.tsx
+++ src/pages/StatusPage/StatusPage.tsx
@@ -14,7 +14,7 @@ export const StatusPage = () => {
   const { transactionId } = useParams();
 
   const { data: transactionData } = useFetch({
-    url: transactionId ? `transaction/${transactionId}/` : "",
+    url: transactionId ? `transactions/${transactionId}/` : "",
     method: "GET",
     queryOptions: {
       queryKey: ["transaction", transactionId],
