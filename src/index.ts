41ab3ab7be90fc53963b8dc7c5dc8ef427a75ead
Celso Cardenas <cardenascelso9@gmail.com>
2025-02-06 13:36:14 -0600

    reorder import and exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import "react-phone-number-input/style.css"
 export { App as BandoWidget } from './App'
 export { useWallets, walletComparator } from './hooks/useWallets';
 export * from './config/versions.js'
 export * from './types/widget.js'
 export * from './types/events.js'
-import "react-phone-number-input/style.css"
