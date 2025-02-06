d91ba22df21597e8ef914d99e00649516bb72f89
Celso Cardenas <cardenascelso9@gmail.com>
2025-02-06 13:01:44 -0600

    include css file on project

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { useWallets, walletComparator } from './hooks/useWallets';
 export * from './config/versions.js'
 export * from './types/widget.js'
 export * from './types/events.js'
+import "./styles.css"
