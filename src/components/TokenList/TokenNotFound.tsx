e23d729d51d60f4f8f17372476a55665507d2787
g6s <guayabas@hey.com>
2025-02-07 15:33:25 -0600

    emit event when insufficient funds or no tokens ar found

--- src/components/TokenList/TokenNotFound.tsx
+++ src/components/TokenList/TokenNotFound.tsx
@@ -4,12 +4,19 @@ import type { FormTypeProps } from '../../stores/form/types.js'
 import { FormKeyHelper } from '../../stores/form/types.js'
 import { useFieldValues } from '../../stores/form/useFieldValues.js'
 import { SearchNotFound } from '../Search/SearchNotFound.js'
+import { useWidgetEvents } from '../../hooks/useWidgetEvents.js'
+import { WidgetEvent, NoTokensAvailable } from '../../types/events.js'
 
 export const TokenNotFound: React.FC<FormTypeProps> = ({ formType }) => {
   const { t } = useTranslation()
   const [selectedChainId] = useFieldValues(FormKeyHelper.getChainKey(formType))
+  const emitter = useWidgetEvents()
   const { getChainById } = useAvailableChains()
 
+  emitter.emit(WidgetEvent.NoTokensAvailable, {
+    chainId: selectedChainId,
+  } as NoTokensAvailable)
+  
   return (
     <SearchNotFound
       message={t('info.message.emptyTokenList', {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "react-phone-number-input/style.css"
 export { App as BandoWidget } from './App'
 export { useWallets, walletComparator } from './hooks/useWallets';
+export { useWidgetEvents, widgetEvents } from './hooks/useWidgetEvents.js'
 export * from './config/versions.js'
 export * from './types/widget.js'
 export * from './types/events.js'
