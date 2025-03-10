46d3b0f12e05bd168870f2797a65953a240cf580
Celso Cardenas <cardenascelso9@gmail.com>
2025-03-10 15:50:24 -0600

    add api query to transaction detail

--- src/pages/TransactionHistory/TransactionDetail.tsx
+++ src/pages/TransactionHistory/TransactionDetail.tsx
@@ -4,13 +4,25 @@ import { useHeader } from "../../hooks/useHeader";
 import { useAccount } from "@lifi/wallet-management";
 import { BottomSheet } from "../../components/BottomSheet/BottomSheet";
 import { Button, Paper } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 
 export const TransactionsDetailPage = () => {
   const { t } = useTranslation();
-  useHeader(t("history.detailTitle"));
   const { account } = useAccount();
+  const { transactionId } = useParams();
 
-  //TODO: query transaction detail
+  useHeader(t("history.detailTitle"));
+
+  const { data: transactionData } = useFetch({
+    url: transactionId ? `transactions/${transactionId}/` : "",
+    method: "GET",
+    queryOptions: {
+      queryKey: ["transaction", transactionId],
+    },
+  });
+
+  console.log(transactionData);
 
   //TODO: query transaction refound
   // if necesary, render refound button
