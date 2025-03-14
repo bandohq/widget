14fb6bcb919fe97ca794ab61672958e4b2c7aabe
Celso Cardenas <cardenascelso9@gmail.com>
2025-03-14 16:13:56 -0600

    clean refunds function

--- src/utils/refunds.ts
+++ src/utils/refunds.ts
@@ -1,31 +1,4 @@
-import { readContract, writeContract } from "@wagmi/core";
-import BandoERC20FulfillableV1 from "@bandohq/contract-abis/abis/BandoERC20FulfillableV1.json";
-
-export const fetchRefunds = async (transactions, config, chain, address) => {
-  const possibleRefunds = transactions.filter(
-    (transaction) => transaction.status === "FAILED"
-  );
-
-  const refundPromises = possibleRefunds.map(async (transaction) => {
-    const FulfillableRegistryABI = BandoERC20FulfillableV1.abi.find(
-      (item) => item.name === "getERC20RefundsFor"
-    );
-
-    const refundAmount = await readContract(config, {
-      address: chain?.protocolContracts?.BandoERC20FulfillableProxy,
-      abi: [FulfillableRegistryABI],
-      functionName: "getERC20RefundsFor",
-      args: [transaction.tokenUsed, address, transaction.serviceId],
-      chainId: chain?.chainId,
-    });
-
-    return { id: transaction.id, amount: refundAmount as BigInt };
-  });
-
-  const refundsArray = await Promise.all(refundPromises);
-
-  return refundsArray;
-};
+import { writeContract } from "@wagmi/core";
 
 export const executeRefund = async ({
   config,
@@ -59,6 +32,6 @@ export const executeRefund = async ({
 
 export const isRefundAvailable = (transactionId, refunds) => {
   return refunds.some(
-    (refund) => refund.id === transactionId && refund.amount > 0
+    (refund) => refund.id === transactionId && refund.txStatus === 13
   );
 };
