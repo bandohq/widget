aea815e25594996a31c4f27af9e432652ca65d28
Celso Cardenas <cardenascelso9@gmail.com>
2025-04-08 23:46:51 -0600

    integrate wallet endpoint

---
+++ src/providers/UserWalletProvider/UserWalletProvider.tsx
@@ -0,0 +1,74 @@
+import React, { createContext, useContext, useMemo, useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { useAccount } from "@lifi/wallet-management";
+import { useWidgetConfig } from "../WidgetProvider/WidgetProvider";
+import { Adress } from "../../pages/SelectChainPage/types";
+
+type WalletInfo = {
+  errorCode?: string;
+  message?: string;
+  wallet?: {
+    hasAcceptedTerms: boolean;
+    walletAddress: Adress;
+  };
+};
+
+type UserWalletContextType = {
+  walletInfo: WalletInfo | null;
+  isPending: boolean;
+  error: Error | null;
+  userAcceptedTermsAndConditions: boolean;
+  setUserAcceptedTermsAndConditions: (value: boolean) => void;
+};
+
+const UserWalletContext = createContext<UserWalletContextType | undefined>(
+  undefined
+);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const UserWalletProvider = ({ children }: Props) => {
+  const { account } = useAccount();
+  const { integrator } = useWidgetConfig();
+  const [userAcceptedTermsAndConditions, setUserAcceptedTermsAndConditions] =
+    useState(false);
+
+  const {
+    data: walletInfo,
+    isPending,
+    error,
+  } = useFetch({
+    url: `wallets/${account.address}/`,
+    method: "GET",
+    queryParams: {
+      integrator: integrator,
+    },
+    queryOptions: {
+      queryKey: ["wallet-info"],
+    },
+  });
+
+  return (
+    <UserWalletContext.Provider
+      value={{
+        walletInfo,
+        isPending,
+        error,
+        userAcceptedTermsAndConditions,
+        setUserAcceptedTermsAndConditions,
+      }}
+    >
+      {children}
+    </UserWalletContext.Provider>
+  );
+};
+
+export const useUserWallet = () => {
+  const context = useContext(UserWalletContext);
+  if (!context) {
+    throw new Error("useUserWallet must be used within a UserWalletProvider");
+  }
+  return context;
+};
