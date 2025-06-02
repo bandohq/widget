a6f70edd7fd886c32581ce89d98dab1a5a8ffda0
Celso Cardenas <cardenascelso9@gmail.com>
2025-06-02 15:35:24 -0600

    adapt translation for single i18n

--- src/pages/StatusPage/SuccessView.tsx
+++ src/pages/StatusPage/SuccessView.tsx
@@ -75,7 +75,12 @@ export const SuccessView = ({ status }) => {
         </StatusTitle>
         <StatusSubtitle fontSize="18px" isCompleted={isStatusCompleted}>
           {t("success.message.notification", {
-            productType: t(`main.${status?.productType}`) || "item",
+            productType:
+              t(
+                status?.productType === "gift_card"
+                  ? `main.${status?.productType}_singular`
+                  : `main.${status?.productType}`
+              ) || "item",
             reference: status?.givenReference || "your reference",
             brand: status?.product?.brand || "",
           })}
