13eaec6cd1d901bcedab42f0a49759105e8a014f
Celso Cardenas <cardenascelso9@gmail.com>
2025-08-14 17:17:26 -0600

    autoselect chain world on first load

--- src/stores/form/FormUpdater.tsx
+++ src/stores/form/FormUpdater.tsx
@@ -4,32 +4,39 @@ import { useWidgetConfig } from "../../providers/WidgetProvider/WidgetProvider.j
 import { formDefaultValues } from "../../stores/form/createFormStore.js";
 import type { DefaultValues } from "./types.js";
 import { useFieldActions } from "./useFieldActions.js";
+import { useWorld } from "../../hooks/useWorld.js";
 
 export const FormUpdater: React.FC<{
   reactiveFormValues: Partial<DefaultValues>;
 }> = ({ reactiveFormValues }) => {
   const { fromChain } = useWidgetConfig();
   const { account } = useAccount();
+  const { isWorld } = useWorld();
   const { isTouched, resetField, setFieldValue, setUserAndDefaultValues } =
     useFieldActions();
 
+  const chainId = isWorld ? 480 : account.chainId;
+
   // Set wallet chain as default if they were not changed during widget usage
   useEffect(() => {
-    if (!account.isConnected || !account.chainId) {
+    if (!account.isConnected || !chainId) {
       return;
     }
 
     if (!fromChain && !isTouched("fromChain") && !isTouched("fromToken")) {
-      resetField("fromChain", { defaultValue: account.chainId });
+      resetField("fromChain", {
+        defaultValue: chainId,
+      });
       setFieldValue("fromToken", "");
       if (isTouched("fromAmount")) {
         setFieldValue("fromAmount", "");
       }
     }
   }, [
-    account.chainId,
+    chainId,
     account.isConnected,
     fromChain,
+    isWorld,
     isTouched,
     resetField,
     setFieldValue,
@@ -38,10 +45,8 @@ export const FormUpdater: React.FC<{
   // Makes widget config options reactive to changes
   // should update userValues when defaultValues updates
   useEffect(() => {
-    setUserAndDefaultValues(
-      accountForChainId(reactiveFormValues, account.chainId)
-    );
-  }, [account.chainId, reactiveFormValues, setUserAndDefaultValues]);
+    setUserAndDefaultValues(accountForChainId(reactiveFormValues, chainId));
+  }, [chainId, reactiveFormValues, setUserAndDefaultValues]);
 
   return null;
 };
